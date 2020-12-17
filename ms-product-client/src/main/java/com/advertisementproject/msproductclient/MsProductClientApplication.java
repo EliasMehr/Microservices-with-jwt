b85f6808ce57a6540b1cc161ab6741a7e47f3203
jeeu1901 <jessie930818@gmail.com>
2020-12-17 15:10:10 +0100

    Added swagger to product-service

--- ms-product-client/src/main/java/com/advertisementproject/msproductclient/MsProductClientApplication.java
+++ ms-product-client/src/main/java/com/advertisementproject/msproductclient/MsProductClientApplication.java
@@ -3,8 +3,12 @@ package com.advertisementproject.msproductclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableSwagger2
 public class MsProductClientApplication {
 
     public static void main(String[] args) {
