45b72ca8db046cd107d98dc1e0617821086f7721
Elias Mehr <55871294+EliasMehr@users.noreply.github.com>
2020-12-18 14:12:07 +0100

    Added .env for enviroment variabls for docker-compose (#13)
    * Added .env for enviroment variabls for docker-compose
    Changed in application.yml to read environment variables through *.env file
    
    * Added ability to forward header downstream to microservices from zuul proxy

--- ms-product-client/src/main/java/com/advertisementproject/msproductclient/api/controllers/ProductController.java
+++ ms-product-client/src/main/java/com/advertisementproject/msproductclient/api/controllers/ProductController.java
@@ -3,12 +3,11 @@ package com.advertisementproject.msproductclient.api.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @RequestMapping("/")
 public class ProductController {
-    
+
     @GetMapping("all")
     public String helloWorld() {
         return "Hello Microservies";
