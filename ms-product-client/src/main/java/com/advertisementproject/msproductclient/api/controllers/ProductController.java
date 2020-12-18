e6d45a073169e2f33501a80d68250e5642c54d5d
EliasMehr <info@eliasmehr.se>
2020-12-18 14:08:46 +0100

    Added ability to forward header downstream to microservices from zuul proxy

--- ms-product-client/src/main/java/com/advertisementproject/msproductclient/api/controllers/ProductController.java
+++ ms-product-client/src/main/java/com/advertisementproject/msproductclient/api/controllers/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class ProductController {
-    
+
     @GetMapping("all")
     public String helloWorld() {
         return "Hello Microservies";
