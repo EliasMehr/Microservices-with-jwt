87b28f5a7b58ed99b887a3b865744a2c74c950da
EliasMehr <info@eliasmehr.se>
2020-12-18 13:46:27 +0100

    Added .env for enviroment variabls for docker-compose Changed in application.yml to read environment variables through *.env file

--- ms-product-client/src/main/java/com/advertisementproject/msproductclient/api/controllers/ProductController.java
+++ ms-product-client/src/main/java/com/advertisementproject/msproductclient/api/controllers/ProductController.java
@@ -3,7 +3,6 @@ package com.advertisementproject.msproductclient.api.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RestController
 @RequestMapping("/")
