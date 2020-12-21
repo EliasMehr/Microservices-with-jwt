4a3a99705096ebb1828cea29d9fe149c1f1bcdd2
EliasMehr <info@eliasmehr.se>
2020-12-21 17:53:35 +0100

    Added lombok dependency to msproductclient

--- ms-product-client/src/main/java/com/advertisementproject/msproductclient/MsProductClientApplication.java
+++ ms-product-client/src/main/java/com/advertisementproject/msproductclient/MsProductClientApplication.java
@@ -3,7 +3,6 @@ package com.advertisementproject.msproductclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import springfox.documentation.annotations.ApiIgnore;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
