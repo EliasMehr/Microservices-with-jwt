3aadf1548cd9f69004dfab2ddbaa15513d70e36b
EliasMehr <info@eliasmehr.se>
2021-01-04 14:47:15 +0100

    Removed test classes for testing jpa persistance

--- zuulgateway/src/main/java/com/advertisementproject/zuulgateway/db/repositories/NewsLetterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.advertisementproject.zuulgateway.db.repositories;
-
-import com.advertisementproject.zuulgateway.db.models.NewsLetter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface NewsLetterRepository extends JpaRepository<NewsLetter, UUID> {
-}
