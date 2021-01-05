ce15332d6956f496249455091e02bcc8b90ce896
jeeu1901 <55871887+jeeu1901@users.noreply.github.com>
2021-01-05 11:41:42 +0100

    Implemented a CORS filter in WebSecurityConfiguration.java to fix CORS error in rest calls from frontend (#19)

--- zuulgateway/src/main/java/com/advertisementproject/zuulgateway/security/configuration/WebSecurityConfiguration.java
+++ zuulgateway/src/main/java/com/advertisementproject/zuulgateway/security/configuration/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.advertisementproject.zuulgateway.security.configuration;
 
 import com.advertisementproject.zuulgateway.security.filters.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +15,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -57,4 +60,20 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
+
 }
