f5ce692204144b381642466d217e1de923894c6e
EliasMehr <info@eliasmehr.se>
2021-01-07 15:16:53 +0100

    Added more categories to CompanyType

--- zuulgateway/src/main/java/com/advertisementproject/zuulgateway/db/models/types/CompanyType.java
+++ zuulgateway/src/main/java/com/advertisementproject/zuulgateway/db/models/types/CompanyType.java
@@ -2,9 +2,9 @@ package com.advertisementproject.zuulgateway.db.models.types;
 
 public enum CompanyType {
     RETAIL,
-    IT,
     TELECOM,
     HEALTH,
     RESTAURANT,
-    
+    TRANSPORTATION,
+    SOFTWARE
 }
