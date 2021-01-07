92d792170a8143cd64e4a50648411f00257582fe
EliasMehr <info@eliasmehr.se>
2021-01-07 15:31:07 +0100

    Removed unused import in entity class user

--- zuulgateway/src/main/java/com/advertisementproject/zuulgateway/db/models/User.java
+++ zuulgateway/src/main/java/com/advertisementproject/zuulgateway/db/models/User.java
@@ -6,12 +6,11 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.UUID;
 
-import static javax.persistence.EnumType.*;
+import static javax.persistence.EnumType.STRING;
 
 @AllArgsConstructor
 @NoArgsConstructor
