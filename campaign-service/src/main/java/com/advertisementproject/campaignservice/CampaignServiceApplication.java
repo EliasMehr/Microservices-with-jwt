1a5d424396838bc7daf7894f95289fc0308af4c3
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2021-01-21 10:29:48 +0100

    Added configurations to application.yml, changed from application.properties Added Dockerfile Added Campaign Service with configurations to docker-compose.yml and removed product-service which is not in use

--- campaign-service/src/main/java/com/advertisementproject/campaignservice/CampaignServiceApplication.java
+++ campaign-service/src/main/java/com/advertisementproject/campaignservice/CampaignServiceApplication.java
@@ -2,8 +2,10 @@ package com.advertisementproject.campaignservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CampaignServiceApplication {
 
     public static void main(String[] args) {
