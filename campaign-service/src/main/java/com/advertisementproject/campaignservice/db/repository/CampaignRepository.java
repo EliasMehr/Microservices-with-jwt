bff40a8de0feca90cddcf3c23fe4dccd05716181
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2021-01-21 11:14:16 +0100

    Added CampaignRepository.java

---
+++ campaign-service/src/main/java/com/advertisementproject/campaignservice/db/repository/CampaignRepository.java
@@ -0,0 +1,12 @@
+package com.advertisementproject.campaignservice.db.repository;
+
+import com.advertisementproject.campaignservice.db.model.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, UUID> {
+
+}
