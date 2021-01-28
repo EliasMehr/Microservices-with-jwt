90e7a9f535a2445ca7b66fc8818566e3e672a0e2
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2021-01-28 10:05:50 +0100

    Added new project account-validation-service Changed application.properties to application.yml and added settings for Eureka, database, mail service and other basic configurations

---
+++ account-validation-service/src/main/java/com/advertisementproject/accountvalidationservice/AccountValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.advertisementproject.accountvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountValidationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountValidationServiceApplication.class, args);
+    }
+
+}
---
+++ account-validation-service/src/test/java/com/advertisementproject/accountvalidationservice/AccountValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advertisementproject.accountvalidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountValidationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
