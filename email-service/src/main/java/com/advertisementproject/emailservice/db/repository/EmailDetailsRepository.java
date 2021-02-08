b241018eaba002da671bedc60d0d8254715d9ee4
daniel-hughes-nackademin <55873880+daniel-hughes-nackademin@users.noreply.github.com>
2021-02-08 10:05:12 +0100

    Update EmailDetailsRepository.java
    Removed unnecessary imports

--- email-service/src/main/java/com/advertisementproject/emailservice/db/repository/EmailDetailsRepository.java
+++ email-service/src/main/java/com/advertisementproject/emailservice/db/repository/EmailDetailsRepository.java
@@ -2,8 +2,6 @@ package com.advertisementproject.emailservice.db.repository;
 
 import com.advertisementproject.emailservice.db.entity.EmailDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -12,7 +10,4 @@ import java.util.UUID;
  * Standard JPA repository for doing CRUD operations in the database for email details
  */
 @Repository
-public interface EmailDetailsRepository extends JpaRepository<EmailDetails, UUID> {
-
-
-}
+public interface EmailDetailsRepository extends JpaRepository<EmailDetails, UUID> {}
