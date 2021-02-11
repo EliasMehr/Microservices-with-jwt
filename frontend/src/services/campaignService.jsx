a837574ca2b170d64fe798419dc1ea0f03c365a8
jeeu1901 <jessie930818@gmail.com>
2021-02-11 20:32:31 +0100

    Fix: Docker now working with frontend

---
+++ frontend/src/services/campaignService.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import headerRequest from "../utils/headerRequest";
+
+const campaignService = {
+  getAllPublishedCampaigns,
+  getDiscountCode,
+  getAllCompanyCampaigns,
+};
+
+function getAllPublishedCampaigns() {
+  return axios
+    .get("http://localhost:8080/campaign/all/published")
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log("ERROR");
+      return err;
+    });
+}
+
+function getDiscountCode(campaignId) {
+  console.log("in discount code " + new headerRequest().Authorization);
+  return axios
+    .get("http://localhost:8080/campaign/discount-code/" + campaignId, {
+      headers: headerRequest(),
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log("ERROR" + err);
+    });
+}
+
+function getAllCompanyCampaigns() {
+  return axios.get("http://localhost:8080/campaign", {
+    headers: headerRequest(),
+  });
+}
+
+export default campaignService;
